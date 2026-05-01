1d97047be8735a8b0f73a1c04d384b6c4bdf1afa
shubhpatil28 <shubhampatil282487@gmail.com>
2026-05-01 12:00:02 +0530

    fix navbar contrast visibility

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   useEffect(() => {
     const handleScroll = () => {
       const navbar = document.querySelector(".navbar");
-      if (window.scrollY > 50) {
+      if (window.scrollY > 10) {
         navbar.classList.add("scrolled");
       } else {
         navbar.classList.remove("scrolled");
